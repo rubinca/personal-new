76ea3faee2ad8959b1efa4ea35ca7a4a2f183c02
Caitlin Rubin <rubinca@sas.upenn.edu>
2021-07-24 23:01:19 -0400

    added a server serving index.html hopefully

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+app.use('/static', express.static("dist"));
+
+const port = normalizePort(process.env.PORT || '3000');
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'content-type': 'text/html' })
+//   fs.createReadStream('index.html').pipe(res)
+// })
+//
+// server.listen(process.env.PORT || 3000)
